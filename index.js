4d8571b98f9d45c455c115664dc0749ea5f430a0
ShawnTownsend <stitans28@gmail.com>
2021-09-28 14:42:49 +0000

    Completed question 1. Didn't complete others.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 //1
 function countToFifty(){
-  
+  for (number = 1; number <= 50; number++) {
+    console.log(number);
+  }
 }
+countToFifty();
 
 //2
 function countFromOne(){
-  
+    
 }
 
 //3
 function threeAndFiveInFifty(){
-  
-}
+    for (let num = 0; num < 50; num++)
+        {    
+            if (num % 3 == 0 && num % 5 == 0)
+            return(num);
+        }
+    }
+threeAndFiveInFifty(); 
 
 //4
 function threeAndFive(){
